ae65a22625ffc48b6a10bddb24e8a46f857725ad
Juan Tuñón <primerproyecto@gmail.com>
2020-03-12 21:59:19 +0100

    agrego bransstripe

---
+++ src/components/BrandsStripe/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as S from './styled';
+
+export const BrandsStripe = () => {
+  return (
+    <div>Aquí irá una lista de brands</div>
+  );
+};
+
+export default BrandsStripe;
